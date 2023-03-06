4623f5c45a7e8990d346b3d27ad884f8d5cc5b02
NPCDALAL <124927029+NPCDALAL@users.noreply.github.com>
2023-03-06 13:12:41 -0700

    n,l;.'

---
+++ main.js
@@ -0,0 +1,13 @@
+
+7// CS10 Grade Calculator
+
+// Button Click Listner
+document.getElementById("btn")addEventListener("click", btnClicked);
+
+function btnClicked() {
+    // Input
+    
+    // PROCESS
+
+    // OUTPUT
+}
